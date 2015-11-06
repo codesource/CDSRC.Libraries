556436cf5952bc3bcddd17305d16f544c0586f36
Matthias Toscanelli <m.toscanelli@code-source.ch>
2015-11-06 02:15:02 +0100

    [TASK] Code refactoring

--- Tests/Unit/Utility/Fixture/DummyClass2.php
+++ Tests/Unit/Utility/Fixture/DummyClass2.php
@@ -1,36 +1,29 @@
 <?php
 namespace CDSRC\Libraries\Tests\Unit\Utility\Fixture;
-/*
- * Copyright (C) 2015 Matthias Toscanelli <m.toscanelli@code-source.ch>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
+    /* *
+     * This script belongs to the TYPO3 Flow package "CDSRC.Libraries".       *
+     *                                                                        *
+     *                                                                        */
+
 
 /**
  * Description of DummyClass
  *
  * @author Matthias Toscanelli <m.toscanelli@code-source.ch>
  */
-class DummyClass2 {
-    
+class DummyClass2
+{
+
     protected $var;
-    
-    public function __construct($var){
+
+    public function __construct($var)
+    {
         $this->var = $var;
     }
-    
-    public function getVar(){
+
+    public function getVar()
+    {
         return $this->var;
     }
 }
